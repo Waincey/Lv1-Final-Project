aaea0a6f22c3dc5548b9c75fbc8ad2392c3c0e2e
EC2 Default User <ec2-user@ip-172-31-0-215.us-east-2.compute.internal>
2018-07-20 16:34:40 +0000

    project

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
+require 'bundler'
+Bundler.require
 require './config/environment'
 require './app/models/sample_model'
 
@@ -10,4 +12,90 @@ class ApplicationController < Sinatra::Base
   get '/' do
     return erb :index
   end
+
+  
+  get '/wouldyourather' do
+    return erb :wouldyourather 
+  end
+  
+  post '/results' do
+    Responder = Rather.new(params)
+    @answer =Responder.results
+    puts params
+    erb :results
+  end
+  
+ get '/ant' do 
+   erb :ant
+ end 
+ get '/cat' do 
+   erb :cat
+ end 
+ get '/dog' do 
+   erb :dog
+ end 
+ get '/duck' do 
+   erb :duck
+ end 
+ get '/frog' do 
+   erb :frog
+ end 
+ get '/lion' do 
+   erb :lion
+ end 
+ get '/monkey' do 
+   erb :monkey
+ end 
+ get '/mouse' do 
+   erb :mouse
+ end 
+ get '/parrot' do 
+   erb :parrot
+ end 
+ get '/rabbit' do 
+   erb :rabbit
+ end 
+ get '/wolf' do 
+   erb :wolf
+ end 
+ get '/animalquiz' do
+    erb :animalquiz
+ end 
+  post '/animalresults' do
+    answers = params.values
+    @user_input = Array.new 
+    answers.each do |answers|
+    @user_input.push(answers) 
+    end 
+    
+    @animals = animalresults(@user_input)
+    if @animals =="Rabbit"
+      erb :rabbit
+    elsif @animals == "Dog"
+      erb :dog
+    elsif @animals == "Cat"
+      erb :cat
+    elsif @animals == "Duck"
+      erb :duck
+    elsif @animals == "Parrot"
+      erb :parrot
+    elsif @animals == "Lion"
+      erb :lion
+    elsif @animals == "Wolf"
+      erb :wolf
+    elsif @animals == "Mouse"
+      erb :mouse
+    elsif @animals == "Frog"
+      erb :frog
+    elsif @animals == "Monkey"
+      erb :monkey
+    else @animals == "Ant"
+      erb :ant
+    end
+  
+  # get '/respond' do
+  #   erb :respond
+  # end
+  
+end
 end
--- app/models/sample_model.rb
+++ app/models/sample_model.rb
@@ -1,3 +1,154 @@
-class SampleModel
-  attr_accessor :name, :age
-end
+class Rather
+    def initialize(params)
+        @params = params
+        @score = {
+            :score => 0
+        }
+    end
+
+    def q1
+     if @params[:speed]=="1"
+        @score[:score]+=1
+     else
+        @score[:score]+=0
+     end
+    end
+    
+    def q2
+     if @params[:prefer]=="1"
+        @score[:score]+=1
+        else
+        @score[:score]+=0
+     end
+    end
+    
+    def q3
+     if @params[:time]=="1"
+        @score[:score]+=1
+     else
+        @score[:score]+=0
+     end
+    end 
+    
+    def q4
+     if @params[:you_are]=="1"
+        @score[:score]+=1
+     else
+        @score[:score]+=0
+     end
+    end
+    
+    def q5
+     if @params[:bathroom]=="1"
+        @score[:score]+=1
+     else
+        @score[:score]+=0
+     end
+    end
+    
+    def results
+        q1
+        q2
+        q3
+        q4
+        q5
+          
+          result = {
+              :selfish =>{
+                  :story => "Often think of yourself more than others."},
+              :selfless =>{
+                  :story => "Have humble and admirable qualities."}
+                  }
+                  
+            if @score[:score] == 3 || @score[:score] >=3
+                answer = result[:selfish][:story]
+            else
+                answer = result[:selfless][:story]
+            end
+    end
+end 
+
+
+
+
+    $rabbit=["A","D","H","J","N"]
+
+    $dog=["C","E","H","K","O"]
+
+    $cat=["C","D","G","L","M"]
+
+    $duck=["B","E","I","J","N"]
+
+    $parrot=["C","F","I","J","N"]
+
+    $lion=["A","D","H","L","N"]
+
+    $wolf=["C","D","H","L","N"]
+
+    $mouse=["A","E","I","L","M"]
+    
+    $frog=["B","F","I","L","O"]
+
+    $monkey=["C","E","G","K","O"]
+    
+ def animalresults(user_input)
+     if $rabbit == user_input
+         "Rabbit"
+    elsif $dog == user_input
+         "Dog"
+         elsif $cat == user_input
+         "Cat"
+         elsif $duck == user_input
+         "Duck"
+         elsif $parrot == user_input
+         "Parrot"
+         elsif $lion == user_input
+         "Lion"
+         elsif $wolf == user_input
+         "Wolf"
+         elsif $mouse == user_input
+         "Mouse"
+         elsif $frog == user_input
+         "Frog"
+        elsif $monkey == user_input
+         "Monkey"
+     else 
+         "Ant"
+     end
+ end
+
+    
+    # if animalresults == ["A","D","H","J","N"]
+    #     answer = "Rabbit"
+        
+    # elsif animalresults == ["C","E","H","K","O"]
+    #     answer = "Dog"
+        
+    # elsif animalresults == ["C","D","G","L","M"]
+    #     answer = "Cat"
+        
+    # elsif animalresults == ["B","E","I","J","N"]
+    #     answer = "Duck"
+        
+    # elsif animalresults == ["C","F","I","J","N"]
+    #     answer = "Parrot"
+        
+    # elsif animalresults == ["A","D","H","L","N"]
+    #     answer = "Lion"
+        
+    # elsif animalresults == ["C","D","H","L","N"]
+    #     answer = "Wolf"
+        
+    # elsif animalresults == ["A","E","I","L","M"]
+    #     answer = "Mouse"
+        
+    # elsif animalresults == ["B","F","I","L","O"]
+    #     answer = "Frog"
+        
+    # elsif animalresults == ["C","E","G","K","O"]
+    #     answer = "Monkey"
+        
+    # else
+    #     answer = "You failed the test somehow, so you are an Ant. Try again!"
+    # end
+    
